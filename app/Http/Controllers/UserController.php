8fb05933e574a39b3245a825733fb04f01656f7a
Natzaum <natacezerbordignon@gmail.com>
2026-02-28 18:10:11 -0300

    feat: Adding get by id route.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UserController extends Controller
 {
-    public function index():JsonResponse
+    public function index(): JsonResponse
     {
         $users = User::orderBy('id', 'DESC')->paginate(2);
 
         return response()->json([
             'status' => true,
             'user' => $users,
-        ]);
+        ], 200);
+    }
+
+    public function show(User $user): JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+            'user' => $user,
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index']); # http://127.0.0.1:8000/api/users?page=1
+Route::get('/users/{user}', [UserController::class, 'show']); # http://127.0.0.1:8000/api/users/1
