53433ac90530176be7d5b961454b92b9c0058718
Natzaum <natacezerbordignon@gmail.com>
2026-03-01 14:26:53 -0300

    feat: Creating user auth and logout.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Exception;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+        if(Auth::attempt([
+            'email' => $request->email, 'password' => $request->password
+        ])) {
+            $user = Auth::user();
+
+            $token = $request->user()->createToken('api-token')->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'token' => $token,
+                'message' => 'Login successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Incorrect credentials'
+            ], 404);
+        }
+    }
+
+    public function logout(User $user): JsonResponse
+    {   
+        try {
+            $user->tokens()->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Logout successfully'
+            ], 200);
+        } catch(Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Cannot logout'
+            ], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,16 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']); # http://127.0.0.1:8000/api/users?page={page}
-Route::get('/users/{user}', [UserController::class, 'show']); # http://127.0.0.1:8000/api/users/{id}
+# Public routes
 Route::post('/users', [UserController::class, 'store']); # http://127.0.0.1:8000/api/users
-Route::put('/users/{user}', [UserController::class, 'update']); # http://127.0.0.1:8000/api/users/{id}
-Route::delete('/users/{user}', [UserController::class, 'destroy']); # http://127.0.0.1:8000/api/users/{id}
+Route::post('/login', [AuthController::class, 'login']); # http://127.0.0.1:8000/api/login
 
-Route::post('/login', [AuthController::class]);
+# Restricted routes
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/users', [UserController::class, 'index']); # http://127.0.0.1:8000/api/users?page={page}
+    Route::get('/users/{user}', [UserController::class, 'show']); # http://127.0.0.1:8000/api/users/{id}
+    Route::put('/users/{user}', [UserController::class, 'update']); # http://127.0.0.1:8000/api/users/{id}
+    Route::delete('/users/{user}', [UserController::class, 'destroy']); # http://127.0.0.1:8000/api/users/{id}
+
+    Route::post('/logout/{user}', [AuthController::class, 'logout']); # http://127.0.0.1:8000/api/logout/{id}
+});
